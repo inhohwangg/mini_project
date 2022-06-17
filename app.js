b89978d23faebf7cdd57613d83dcd7f7895c6e85
SEOCL <com5045@gmail.com>
2022-06-17 21:02:54 +0900

    [fetch] 보안 라이브러리 추가
    [fetch] 보안 라이브러리 추가

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-const express = require('express')
-const app = require('express')()
-const {sequelize} = require('./models')
+const express = require("express")
+const app = require("express")()
+const hpp = require("hpp")
+const helmet = require("helmet")
+const morgan = require("morgan")
+const { sequelize } = require("./models")
 
 //MySQL Sequelize 연결
 sequelize
-    .sync({force: false})
-    .then(()=> {
-        console.log("데이터베이스 연결 성공")
-    })
-    .catch((err)=> {
-        console.log(err, "데이터베이스 연결 실패")
-    })
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공")
+  })
+  .catch(err => {
+    console.log(err, "데이터베이스 연결 실패")
+  })
 
 //라우터 불러오기
-const userRouter = require('./routes/user')
-const myprofileRouter = require('./routes/myprofile')
-
+const userRouter = require("./routes/user")
+const myprofileRouter = require("./routes/myprofile")
 
 //각종 미들웨어
 app.use(express.json())
 app.use(express.urlencoded())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
+app.use(hpp())
+app.use(helmet())
+app.use(morgan("short"))
 
 //라우터 연결
-app.use("/api", [
-    userRouter,
-    myprofileRouter
-])
+app.use("/api", [userRouter, myprofileRouter])
 
 //서버 실행
-app.listen(3000, ()=> {
-    console.log("3000포트로 서버가 켜졌습니다.")
+app.listen(3000, () => {
+  console.log("3000포트로 서버가 켜졌습니다.")
 })
