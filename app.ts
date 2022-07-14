65621cec860f8b0bd85d6e710dc4a9948f045d1d
inhohwangg <101077035+inhohwangg@users.noreply.github.com>
2022-07-14 23:10:11 +0900

    typescript dummy api add

--- app.ts
+++ app.ts
@@ -1,22 +1,24 @@
-"use strict";
 var express = require("express");
 const app = require("express")();
 const helmet = require("helmet");
 const hpp = require("hpp");
 const { sequelize } = require("./models");
 const cors = require("cors");
+
 //MySQL Sequelize 연결
 sequelize
-    .sync({ force: false })
-    .then(() => {
+  .sync({ force: false })
+  .then(() => {
     console.log("데이터베이스 연결 성공");
-})
-    .catch((err) => {
+  })
+  .catch((err:any) => {
     console.log(err, "데이터베이스 연결 실패");
-});
+  });
+
 //라우터 불러오기
-const videoRouter = require("./routes/videoR");
-// const infoInsertRouter = require("./routes/infoInsert");
+const videoRouter = require("./routes/video");
+const infoInsertRouter = require("./routes/infoInsert");
+
 //각종 미들웨어
 app.use(express.json());
 app.use(express.urlencoded());
@@ -25,15 +27,19 @@ app.use(helmet());
 app.use(hpp());
 // app.use(morgan('combined'))
 app.use(cors());
+
 //라우터 연결
-app.use("/api", [videoRouter]); //, infoInsertRouter
-app.use("/", (req, res) => {
-    res.send("<h1>안녕하세요</h1>");
+app.use("/api", [videoRouter, infoInsertRouter]);
+
+app.use("/", (req:any, res:any) => {
+  res.send("<h1>안녕하세요</h1>");
 });
+
 //서버 실행!
 app.listen(3000, () => {
-    console.log("3000포트로 서버가 켜졌습니다.");
+  console.log("3000포트로 서버가 켜졌습니다.");
 });
+
 app.listen(443, () => {
-    console.log("443포트로 서버가 켜졌습니다.");
+  console.log("443포트로 서버가 켜졌습니다.");
 });
