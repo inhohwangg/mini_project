35cb83577a607ef551335d03fe4abd8fdff838fb
서현우 <101075938+SEOCL@users.noreply.github.com>
2022-07-18 17:49:19 +0900

    [fetch] 한국 비디오 API 추가
    [fetch] 한국 비디오 API 추가

---
+++ routes/videoR.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+const videoUrlResJ = require("../controllers/videoC");
+const videoUrlResG = require("../controllers/videoC");
+
+require("dotenv").config();
+
+// 일본 영상 내리는 GET API
+router.get("/videoj", videoUrlResJ.VideoURLJ);
+
+// 독일 영상 내리는 GET API
+router.get("/videog", videoUrlResG.VideoURLG);
+
+module.exports = router;
