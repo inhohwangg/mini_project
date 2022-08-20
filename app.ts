969df017cd038d6420d838cfd5f4191a678cfabe
inhohwangg <sk1440sk@naver.com>
2022-08-20 13:47:16 +0900

    lib morgan install

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ var express = require("express");
 const app = require("express")();
 const helmet = require("helmet");
 const hpp = require("hpp");
+const morgan = require('morgan')
 const { sequelize } = require("./models");
 const cors = require("cors");
 
@@ -25,7 +26,7 @@ app.use(express.urlencoded());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(hpp());
-// app.use(morgan('combined')) --> 안쓰는 건가요??
+app.use(morgan('combined'))
 app.use(cors());
 
 //라우터 연결
